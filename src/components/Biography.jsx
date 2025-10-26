1ac1f4cf509ae5ed559aafa513da7e95240130cb
PabloMelzi <paul281404@gmail.com>
2025-10-26 19:55:54 +0100

    Responsive 3.0

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -11,11 +11,11 @@ const Biography = () => {
           <h4 className={styles.question}>¿Quién va a entrenarte?</h4>
           
           <p className={styles.description}>
-            Soy Nacho Morales Puerto, licenciado en Ed. Física en EADE, preparador físico, entrenador de
+            Soy <strong>Nacho Morales Puerto</strong>, licenciado en Ed. Física en EADE, preparador físico, entrenador de
             tecnificación, entrenador de baloncesto con una experiencia de 5 años en el sector, he
-            practicado baloncesto durante más de 14 años como jugador, llegando a disputar fases de
-            ascenso a liga nacional. Actualmente me dedico a preparar a jugadores para conseguir su
-            máximo rendimiento en la pista a partir de un trabajo fuera de ella.
+            practicado baloncesto durante más de 14 años como jugador, llegando a disputar <strong>fases de
+            ascenso</strong> a liga nacional. Actualmente me dedico a <strong>preparar a jugadores</strong> para conseguir su
+            máximo rendimiento en la pista a partir de un trabajo fuera de ella. Además de ayudar a personas a <strong>conseguir sus objetivos</strong> tanto de rendimiento como de estética (al mas puro estilo personal trainer).
           </p>
           
           <blockquote className={styles.quote}>
